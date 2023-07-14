c6901a3428c37f759e3bae1cf03e0794eb5b4e8a
Tiago França <tiago@tiagofranca.com>
2023-07-13 22:53:37 -0300

    enabled breeze dark
    	modified:   .gitignore
    	new file:   bootstrap/ssr/.gitignore
    	modified:   composer.lock
    	modified:   resources/js/Components/Checkbox.vue
    	modified:   resources/js/Components/DangerButton.vue
    	modified:   resources/js/Components/Dropdown.vue
    	modified:   resources/js/Components/DropdownLink.vue
    	modified:   resources/js/Components/InputError.vue
    	modified:   resources/js/Components/InputLabel.vue
    	modified:   resources/js/Components/Modal.vue
    	modified:   resources/js/Components/NavLink.vue
    	modified:   resources/js/Components/PrimaryButton.vue
    	modified:   resources/js/Components/ResponsiveNavLink.vue
    	modified:   resources/js/Components/SecondaryButton.vue
    	modified:   resources/js/Components/TextInput.vue
    	modified:   resources/js/Layouts/AuthenticatedLayout.vue
    	modified:   resources/js/Layouts/GuestLayout.vue
    	modified:   resources/js/Pages/Auth/ConfirmPassword.vue
    	modified:   resources/js/Pages/Auth/ForgotPassword.vue
    	modified:   resources/js/Pages/Auth/Login.vue
    	modified:   resources/js/Pages/Auth/Register.vue
    	modified:   resources/js/Pages/Auth/VerifyEmail.vue
    	modified:   resources/js/Pages/Dashboard.vue
    	modified:   resources/js/Pages/Profile/Edit.vue
    	modified:   resources/js/Pages/Profile/Partials/DeleteUserForm.vue
    	modified:   resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
    	modified:   resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue

--- resources/js/Pages/Profile/Partials/DeleteUserForm.vue
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.vue
@@ -40,9 +40,9 @@ const closeModal = () => {
 <template>
     <section class="space-y-6">
         <header>
-            <h2 class="text-lg font-medium text-gray-900">Delete Account</h2>
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Delete Account</h2>
 
-            <p class="mt-1 text-sm text-gray-600">
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
                 Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting
                 your account, please download any data or information that you wish to retain.
             </p>
@@ -52,11 +52,11 @@ const closeModal = () => {
 
         <Modal :show="confirmingUserDeletion" @close="closeModal">
             <div class="p-6">
-                <h2 class="text-lg font-medium text-gray-900">
+                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
                     Are you sure you want to delete your account?
                 </h2>
 
-                <p class="mt-1 text-sm text-gray-600">
+                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
                     Once your account is deleted, all of its resources and data will be permanently deleted. Please
                     enter your password to confirm you would like to permanently delete your account.
                 </p>
--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -25,9 +25,9 @@ const form = useForm({
 <template>
     <section>
         <header>
-            <h2 class="text-lg font-medium text-gray-900">Profile Information</h2>
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Profile Information</h2>
 
-            <p class="mt-1 text-sm text-gray-600">
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
                 Update your account's profile information and email address.
             </p>
         </header>
@@ -65,13 +65,13 @@ const form = useForm({
             </div>
 
             <div v-if="mustVerifyEmail && user.email_verified_at === null">
-                <p class="text-sm mt-2 text-gray-800">
+                <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
                     Your email address is unverified.
                     <Link
                         :href="route('verification.send')"
                         method="post"
                         as="button"
-                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                     >
                         Click here to re-send the verification email.
                     </Link>
@@ -79,7 +79,7 @@ const form = useForm({
 
                 <div
                     v-show="status === 'verification-link-sent'"
-                    class="mt-2 font-medium text-sm text-green-600"
+                    class="mt-2 font-medium text-sm text-green-600 dark:text-green-400"
                 >
                     A new verification link has been sent to your email address.
                 </div>
@@ -94,7 +94,7 @@ const form = useForm({
                     leave-active-class="transition ease-in-out"
                     leave-to-class="opacity-0"
                 >
-                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
+                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600 dark:text-gray-400">Saved.</p>
                 </Transition>
             </div>
         </form>
